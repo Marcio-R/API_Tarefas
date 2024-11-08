fc6a46b1093a75071bdd410ee99bfd53bca0c80c
marcio ronaldo alves <marcinho-ronald@hotmail.com>
2024-11-07 21:07:31 -0300

    Add project files.

---
+++ ApiTarefas/Data/ApiTarefasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiTarefas.Model;
+
+namespace ApiTarefas.Data
+{
+    public class ApiTarefasContext : DbContext
+    {
+        public ApiTarefasContext (DbContextOptions<ApiTarefasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiTarefas.Model.Tarefa> Tarefa { get; set; } = default!;
+    }
+}
---
+++ ApiTarefas/Migrations/20241107235345_inicial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTarefas.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Concluida = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefa", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefa");
+        }
+    }
+}
